4e8ea4d78d77f3d3eda6f9eed8f2796363eaf479
David Rajcher (Codaisseur) <davidrajchercodaisseur@Davids-MBP.codaisseur.local>
2022-04-14 14:57:05 +0200

    exerice done

---
+++ client.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const URL = "http://localhost:3000";
+
+const welcomeMessage = async () => {
+  try {
+    const res = await axios.get(`${URL}/`);
+    const message = res.data;
+
+    console.log(message);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// NOTICE: most those tasks require changes to server.js (backend) as well
+
+// TASK: request all characters
+
+// TASK: request one character using id
+
+// TASK request all character of a specific house
+
+// TASK request the future age of characters, passing the number of years from now
+
+// Run 'node client.js' to execute these functions
+welcomeMessage();
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const characters = require("./characters.json");
+const houses = [
+  "Stark",
+  "Arryn",
+  "Baratheon",
+  "Greyjoy",
+  "Lannister",
+  "Martell",
+];
+const app = express();
+
+// GET hello player
+app.get("/", (req, res) => {
+  res.send("Welcome to Game of Thrones");
+});
+
+// GET all characters
+app.get("/characters", (req, res) => {
+  res.send(characters);
+});
+
+// GET find character by id
+app.get("/characters/:id", (req, res) => {
+  const { id } = req.params;
+
+  // BUG: no user is found!
+  const character = characters.find((character) => character.id === id);
+
+  // BONUS TASK: send an error if no user was found
+
+  res.send(character);
+});
+
+// GET filter character by house
+app.get("/house/:name", (req, res) => {
+  const { id } = req.params;
+
+  // TASK: filter users by house
+
+  // TASK: users should be found even if we type the house name with different cases
+  // so "Stark" or "STArk" or "STARK" or "strak" should be the same :)
+
+  // BONUS TASK: send an error if there are no users
+
+  // BONUS TASK: check if house given is in the list of houses
+  // If it is not in the list, send an error saying: "incorrect house name"
+  // The house list is on top
+
+  res.send(character);
+});
+
+// BONUS!!!
+// GET characters age in ... years
+// this should check if the character is alive
+// if it is alive, add the years to its age
+// send back characters with the updated age
+app.get("/future/:years", (req, res) => {});
+
+PORT = 3000;
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
